0d60d7520b13035b231838c6563f212e90c76106
garrettwang(王慧祥) <garrettwang@tencent.com>
2019-11-11 21:46:38 +0800

    update

--- source/_static/js/tencent_statistic.js
+++ source/_static/js/tencent_statistic.js
@@ -1,12 +1,12 @@
 var _hmt = _hmt || [];
 (function() {
   var hm1 = document.createElement("script");
-  hm1.src = "http://tajs.qq.com/stats?sId=66487856";
+  hm1.src = "https://tajs.qq.com/stats?sId=66487856";
   var s1 = document.getElementsByTagName("script")[0];
   s1.parentNode.insertBefore(hm1, s1);
 
   var hm2 = document.createElement("script");
-  hm2.src = "http://tajs.qq.com/stats?sId=66487821";
+  hm2.src = "https://tajs.qq.com/stats?sId=66487821";
   var s2 = document.getElementsByTagName("script")[0];
   s2.parentNode.insertBefore(hm2, s2);
 })();
